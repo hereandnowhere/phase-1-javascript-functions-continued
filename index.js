f34cbd31b2909006866d7288bdb9aab36be80267
Jacob Vine <jvine1235@gmail.com>
2022-02-03 12:14:18 -0500

    ?Completed assignment? really struggled with these to pass the tests. Will follow up later with you Laura.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+// I THINK THERE IS A BUG IN THE TESTS, THIS WORKS PERFECTLY IN REPLIT, AND ITS DEMONSTRATING 
+// WHAT WE LEARNED IN THIS MODULE
+// I TRULY DON'T KNOW WHAT I AM DOING WRONG...I'VE WASTED TO MUCH TIME IN THIS LAB JUST TRYING TO DEBUG AND TRY DIFFERENT METHODS WITH THIS
+// I GOT TO MOVE ON BUT I WILL BRING IT UP WITH YOU LATER LAURA
+
+
+const saturdayFun = function(satActivity = "roller-skate"){
+    console.log("This Saturday, I want to " + `${satActivity}!`);
+    return;
+}
+
+console.log(saturdayFun());
+
+const mondayWork = function(monActivity = "go to the office"){
+    console.log(`This Monday, I will ${monActivity}.`);
+    return;
+}
+
+mondayWork();
+
+function wrapAdjective(emphasis = "*"){
+    const part1 = "You are ";
+    return function(emphatic = "special"){
+        const part2 = emphatic;
+        return console.log(`${part1} ${emphasis} ${part2} ${emphasis}!`);
+    };
+}
+
+wrapAdjective()();
